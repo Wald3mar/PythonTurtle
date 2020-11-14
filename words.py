b68d0e79deabc12e8c005aec21bee39ae20b3852
Vladimir Pakhmutov <wwald3mar@mail.ru>
2020-11-14 15:09:56 +0300

    Cnanges

--- words.py
+++ words.py
@@ -24,6 +24,8 @@ turtle.goto(((-text_counter)/2) * 70, 0)
 for i in range(text_counter):
     if text[i] == 'a':
         turtle.goto(0, -50)
+        cx = turtle.xcor()
+        cy = turtle.ycor()
         turtle.pendown()
         #drawing
         x = math.degrees(math.atan(25/100))
@@ -38,10 +40,31 @@ for i in range(text_counter):
         turtle.right(90 - x)
         turtle.forward(25)
         turtle.left(90 - x)
-        turtle.forward(20)
+        turtle.forward(26.5)
         turtle.left(90 - x)
         turtle.forward(25)
         turtle.right(90 - x)
         turtle.forward(15)
+        turtle.penup()
+        turtle.right(180)
+        turtle.forward(15)
+        turtle.left(90 - x)
+        turtle.forward(40)
+        turtle.pendown()
+        turtle.forward(38)
+        turtle.right(180 - 2 * x)
+        turtle.forward(38)
+        turtle.right(90 + x)
+        turtle.forward(16)
+        #end drawing
+        turtle.penup()
+        turtle.setpos(cx, cy)
+        turtle.right(180)
+        if i < text_counter - 1:
+            turtle.goto(90, 0)
+    if text[i] == 'b':
+        turtle.pendown()
+        turtle.left(90)
+        turtle.forward(50)
 #does not exit while not click
 turtle.exitonclick()
