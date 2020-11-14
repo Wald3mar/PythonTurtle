78773d2e0541efd9aad79629705a1d1678f767ef
Vladimir Pakhmutov <wwald3mar@mail.ru>
2020-11-14 14:38:06 +0300

    Cnanges

--- words.py
+++ words.py
@@ -1,44 +1,47 @@
 import turtle
+import math
 
+#screen and pen settings
+turtle.bgcolor("#fcdf74")
+turtle.pencolor("#8D65D8")
+turtle.fillcolor("#5D24C8")
+turtle.pensize(5)
 turtle.title('Word Turtle Program')
-text = turtle.textinput("Turtle.Word", "Введите слово:")
 turtle.penup()
 turtle.hideturtle()
+
+#input
+text = (turtle.textinput("Turtle.Word", "Введите слово:")).lower()
 text_counter = len(text)
-width = 100 * 2 + text_counter * 50 + (text_counter - 1) * 10
+
+#screen size and start position
+width = 100 * 2 + text_counter * 70 + (text_counter - 1) * 20
 height = 300
-turtle.screensize(width, height)
-turtle.setup(width=width,height=height,startx=(-1000 // 2 + 100),starty=(height // 2 - 50))
-for i in text:
-    if i == 'h':
+turtle.setup(width=width,height=height,startx=0,starty=0)
+turtle.goto(((-text_counter)/2) * 70, 0)
+
+#start programm 
+for i in range(text_counter):
+    if text[i] == 'a':
+        turtle.goto(0, -50)
         turtle.pendown()
-        turtle.begin_fill()
-        turtle.left(90)
-        turtle.forward(50)
-        turtle.right(90)
+        #drawing
+        x = math.degrees(math.atan(25/100))
+        turtle.rt(x - 90)
+        turtle.forward(math.sqrt(10625))
+        turtle.right(90 - x)
         turtle.forward(20)
-        turtle.right(90)
-        turtle.forward(40)
-        turtle.left(90)
-        turtle.forward(30)
-        turtle.left(90)
-        turtle.forward(40)
-        turtle.right(90)
+        turtle.right(90 - x)
+        turtle.forward(math.sqrt(10625))
+        turtle.right(90 + x)
+        turtle.forward(15)
+        turtle.right(90 - x)
+        turtle.forward(25)
+        turtle.left(90 - x)
         turtle.forward(20)
-        turtle.right(90)
-        turtle.forward(100)
-        turtle.right(90)
-        turtle.forward(20)
-        turtle.right(90)
-        turtle.forward(40)
-        turtle.left(90)
-        turtle.forward(30)
-        turtle.left(90)
-        turtle.forward(40)
-        turtle.right(90)
-        turtle.forward(20)
-        turtle.right(90)
-        turtle.forward(50)
-        turtle.end_fill()
-        turtle.penup()
+        turtle.left(90 - x)
+        turtle.forward(25)
+        turtle.right(90 - x)
+        turtle.forward(15)
+#does not exit while not click
 turtle.exitonclick()
